148b9e4b5b96e639cc07ee434970288932b08a93
Joshua Wu <jhwu1@buffs.wtamu.edu>
2024-04-21 15:13:39 -0500

    Assignment6 submission

---
+++ Assignment5/barky/barkyapi/key.py
@@ -0,0 +1,2 @@
+
+SECURITY_KEY = 'django-insecure-@us!8)d&r^)zrm35-4p#@+)c$7r6dzz=l62*2w6bm5o$_l7gbp'
--- Assignment6/barky/barky/settings.py
+++ Assignment6/barky/barky/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = key.SECURITY_KEY
+SECRET_KEY = 'django-insecure-@us!8)d&r^)zrm35-4p#@+)c$7r6dzz=l62*2w6bm5o$_l7gbp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'barkyapi.apps.BarkyapiConfig',
+    
 
 ]
 
