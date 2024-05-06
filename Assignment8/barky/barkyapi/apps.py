92e487ba8916eebd874282115b19fa6a2038c76c
Joshua Wu <jhwu1@buffs.wtamu.edu>
2024-05-05 22:26:55 -0500

    initial submission

--- Assignment8/barky/barkyapi/apps.py
+++ Assignment8/barky/barkyapi/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class BarkyapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'barkyapi'
-    
-    def ready(self):
-        import barkyapi.signals
